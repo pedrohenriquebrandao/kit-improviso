28d8ec6ddabb638763c5596394a7c8d9fef7bead
Pedro Brandão <phbrandao@outlook.com>
2023-10-27 17:54:11 -0300

    Ajustando estilização no sorteio de players

--- resources/views/draft/players.blade.php
+++ resources/views/draft/players.blade.php
@@ -2,13 +2,14 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="container p-2 mt-6 mx-auto sm:p-4 text-gray-100">
-                <div class="overflow-x-auto" style="margin-left:2rem">
+                <div class="overflow-x-auto">
                     <button onclick="draft()" class="px-12 py-2 mb-4 font-semibold rounded bg-green-500 text-white" type="button">SORTEAR PLAYER</button>
                 </div>
             </div>
         </div>
     </div>
-        <h1 id="player" style="color: black; font-size: 8rem"></h1>
+        <h1 id="player" class="uppercase" style="color: black; font-size: 8rem"></h1>
+        <button id="refresh" onclick="location.reload(true);" class="hidden px-12 py-2 mb-4 font-semibold rounded bg-blue-500 text-white" type="button">Reiniciar?</button>
 </x-guest-layout>
 
 <script>
@@ -22,9 +23,12 @@
 
         const index = players.indexOf(random);
         players.splice(index, 1);
+        console.log(players);
 
         if(players.length < 1) {
             alert("Acabaram os jogadores");
+            document.getElementById("player").innerHTML =  "";
+            document.getElementById("refresh").style.display = "block";
         }
     }
 </script>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,9 +22,9 @@
                 </a>
             </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{-- <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"> --}}
                 {{ $slot }}
-            </div>
+            {{-- </div> --}}
         </div>
     </body>
 </html>
